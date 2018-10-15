0552d64f5110db19f32cb93db532123fc17118d4
goatez <whfrederick3@gmail.com>
2018-10-15 12:54:06 -0400

    Update wikidata_query-lakes.py

--- wikidata_query-lakes.py
+++ wikidata_query-lakes.py
@@ -1,13 +1,18 @@
 # https://rdflib.github.io/sparqlwrapper/
 
+from SPARQLWrapper import SPARQLWrapper, JSON
+
 # wikidata query for all the lakes in the US
 sparql = SPARQLWrapper("https://query.wikidata.org/sparql")
-sparql.setQuery("""SELECT ?instance_of ?instance_ofLabel ?country ?countryLabel WHERE {
-  SERVICE wikibase:label { bd:serviceParam wikibase:language "[AUTO_LANGUAGE],en". }
-  ?instance_of wdt:P31 wd:Q23397.
-  OPTIONAL { ?instance_of wdt:P17 ?country. }
-}
-LIMIT 100""")
+sparql.setQuery("""#List of all the lakes in US
+SELECT ?lakeLabel
+?lake ?GNIS
+WHERE {
+  ?lake (wdt:P31/wdt:P279*) wd:Q23397.
+  ?lake wdt:P17 wd:Q30.
+  OPTIONAL { ?lake wdt:P1566 ?GNIS. }
+SERVICE wikibase:label { bd:serviceParam wikibase:language "[AUTO_LANGUAGE],en". }
+}""")
 sparql.setReturnFormat(JSON)
 results = sparql.query().convert()
 
