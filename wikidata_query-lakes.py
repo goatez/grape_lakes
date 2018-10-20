d8d84646737a29d2c3a5f51562c85acb869f9737
goatez <whfrederick3@gmail.com>
2018-10-20 17:50:03 -0400

    updated wikidata query

--- wikidata_query-lakes.py
+++ wikidata_query-lakes.py
@@ -5,14 +5,22 @@ from SPARQLWrapper import SPARQLWrapper, JSON
 # wikidata query for all the lakes in the US
 sparql = SPARQLWrapper("https://query.wikidata.org/sparql")
 sparql.setQuery("""#List of all the lakes in US
-SELECT ?lakeLabel
-?lake ?GNIS
+PREFIX schema: <http://schema.org/>
+SELECT ?lake ?article ?coordinate_location ?GNIS_ID ?GeoNames_ID ?lake_inflows ?lake_outflow
 WHERE {
   ?lake (wdt:P31/wdt:P279*) wd:Q23397.
   ?lake wdt:P17 wd:Q30.
-  OPTIONAL { ?lake wdt:P1566 ?GNIS. }
-SERVICE wikibase:label { bd:serviceParam wikibase:language "[AUTO_LANGUAGE],en". }
-}""")
+  SERVICE wikibase:label { bd:serviceParam wikibase:language "[AUTO_LANGUAGE],en". }
+  OPTIONAL { ?article schema:about ?lake.
+             ?article schema:inLanguage "en".
+             ?article schema:isPartOf <https://en.wikipedia.org/>. }
+  OPTIONAL { ?lake wdt:P625 ?coordinate_location. }
+  OPTIONAL { ?lake wdt:P590 ?GNIS_ID. }
+  OPTIONAL { ?lake wdt:P1566 ?GeoNames_ID. }
+  OPTIONAL { ?lake wdt:P200 ?lake_inflows. }
+  OPTIONAL { ?lake wdt:P201 ?lake_outflow. }
+}
+LIMIT 10""")
 sparql.setReturnFormat(JSON)
 results = sparql.query().convert()
 
