72c55329a1bf8e79c4fd32373d59891f2c090367
billy frederick <35885365+goatez@users.noreply.github.com>
2018-10-24 19:56:48 -0400

    update wikimedia_api.py

--- wikimedia_api.py
+++ wikimedia_api.py
@@ -12,9 +12,11 @@ os.getcwd()
 # format = "&format="
 # example:  ?action=query&titles=Lake_Erie&prop=info&format=json
 
-url = "https://en.wikipedia.org/w/api.php?action=query&titles=Lake_Erie&prop=info&format=json"
+url = "https://en.wikipedia.org/w/api.php?action=query&prop=revisions&rvprop=content&format=json&titles=Lake_Michigan&rvsection=0"
+url2 = "https://en.wikipedia.org/w/api.php?action=query&prop=extracts&titles=Lake_Michigan&format=json"
+url3 = "https://en.wikipedia.org/w/api.php?action=query&prop=revisions&rvprop=content&rvsection=0&titles=Lake_Michigan&format=json"
 
-address = urllib.request.urlopen(url)
+address = urllib.request.urlopen(url3)
 data = address.read().decode()
 wiki_lake = json.loads(data)
 
