7b7e8c38c487a25d19e7f003f5ba29c12f9dce3b
billy frederick <35885365+goatez@users.noreply.github.com>
2018-11-04 23:41:26 -0500

    Update

--- wikidata_query_lakes.py
+++ wikidata_query_lakes.py
@@ -48,71 +48,87 @@ for lake in results["results"]["bindings"]:
         lake_dict[lake["lake"]["value"].strip('http://www.wikidata.org/entity/')] = {"lake_name" : lake["lakeLabel"]["value"]}
     except:
         lake_dict[lake["lake"]["value"].strip('http://www.wikidata.org/entity/')] = {"lake_name" : None }
+
     try:  # wikipedia URL
         lake_dict[lake["lake"]["value"].strip('http://www.wikidata.org/entity/')].update({ "wikipedia_link" : lake["article"]["value"] })
     except:
         lake_dict[lake["lake"]["value"].strip('http://www.wikidata.org/entity/')].update({ "wikipedia_link" : None })
+
     try: # wikidata URL
         lake_dict[lake["lake"]["value"].strip('http://www.wikidata.org/entity/')].update({ "wikidata_link" : lake["lake"]["value"] })
     except:
         lake_dict[lake["lake"]["value"].strip('http://www.wikidata.org/entity/')].update({ "wikidata_link" : None })
+
     try: # coordinates
         c = list(map(float, lake["coordinate_location"]["value"].strip('Point()').split()))  # striping string to be cast to a float
         coord = [{"lat" : c[0], "long" : c[1]}, c]  # creating dictionary of lat/long key pairing, and a list
         lake_dict[lake["lake"]["value"].strip('http://www.wikidata.org/entity/')].update({ "coordinates" : coord })
     except:
         lake_dict[lake["lake"]["value"].strip('http://www.wikidata.org/entity/')].update({ "coordinates" : None })
+
     try:  # lake inflows
         lake_dict[lake["lake"]["value"].strip('http://www.wikidata.org/entity/')]\
         .update({ "lake_inflow" : lake["lake_inflows"]["value"].strip('http://www.wikidata.org/entity/') })
     except:
         lake_dict[lake["lake"]["value"].strip('http://www.wikidata.org/entity/')].update({ "lake_inflow" : None })
+
     try: # lake outflows
         lake_dict[lake["lake"]["value"].strip('http://www.wikidata.org/entity/')]\
         .update({ "lake_outflow" : lake["lake_outflow"]["value"].strip('http://www.wikidata.org/entity/') })
     except:
         lake_dict[lake["lake"]["value"].strip('http://www.wikidata.org/entity/')].update({ "lake_outflow" : None })
+
     try:  # elevation above sea level
         lake_dict[lake["lake"]["value"].strip('http://www.wikidata.org/entity/')].update({ "elevation" : int(lake["elevation_above_sea_level"]["value"]) })
     except:
         lake_dict[lake["lake"]["value"].strip('http://www.wikidata.org/entity/')].update({ "elevation" : None })
+
     try:  # area
         lake_dict[lake["lake"]["value"].strip('http://www.wikidata.org/entity/')].update({ "area" : int(lake["area"]["value"]) })
     except:
         lake_dict[lake["lake"]["value"].strip('http://www.wikidata.org/entity/')].update({ "area" : None })
+
     try: # length
         lake_dict[lake["lake"]["value"].strip('http://www.wikidata.org/entity/')].update({ "length" : int(lake["length"]["value"]) })
     except:
         lake_dict[lake["lake"]["value"].strip('http://www.wikidata.org/entity/')].update({ "length" : None })
+
     try:  # width
         lake_dict[lake["lake"]["value"].strip('http://www.wikidata.org/entity/')].update({ "width" : int(lake["width"]["value"]) })
     except:
         lake_dict[lake["lake"]["value"].strip('http://www.wikidata.org/entity/')].update({ "width" : None })
+
     try: # volume as quantity
         lake_dict[lake["lake"]["value"].strip('http://www.wikidata.org/entity/')].update({ "volume" : int(lake["volume_as_quantity"]["value"]) })
     except:
         lake_dict[lake["lake"]["value"].strip('http://www.wikidata.org/entity/')].update({ "volume" : None })
+
     try:  # watershed area
         lake_dict[lake["lake"]["value"].strip('http://www.wikidata.org/entity/')].update({ "watershed" : int(lake["watershed_area"]["value"]) })
     except:
         lake_dict[lake["lake"]["value"].strip('http://www.wikidata.org/entity/')].update({ "watershed" : None })
+
     try:  # perimeter
         lake_dict[lake["lake"]["value"].strip('http://www.wikidata.org/entity/')].update({ "perimeter" : int(lake["perimeter"]["value"]) })
     except:
         lake_dict[lake["lake"]["value"].strip('http://www.wikidata.org/entity/')].update({ "perimeter" : None })
+
     try: # residence time of water
         lake_dict[lake["lake"]["value"].strip('http://www.wikidata.org/entity/')].update({ "residence_time_of_water" : int(lake["residence_time_of_water"]["value"]) })
     except:
         lake_dict[lake["lake"]["value"].strip('http://www.wikidata.org/entity/')].update({ "residence_time_of_water" : None })
+
     try:  # vertical depth
         lake_dict[lake["lake"]["value"].strip('http://www.wikidata.org/entity/')].update({ "vertical_depth" : int(lake["vertical_depth"]["value"]) })
     except:
         lake_dict[lake["lake"]["value"].strip('http://www.wikidata.org/entity/')].update({ "vertical_depth" : None })
+
     try:  # GNIS ID
         lake_dict[lake["lake"]["value"].strip('http://www.wikidata.org/entity/')].update({ "gnis_id" : lake["GNIS_ID"]["value"] })
     except:
         lake_dict[lake["lake"]["value"].strip('http://www.wikidata.org/entity/')].update({ "gnis_id" : None })
-    try:  # Geo-Name ID
+
+    try:  # GeoName ID
         lake_dict[lake["lake"]["value"].strip('http://www.wikidata.org/entity/')].update({ "geoname_id" : lake["GeoNames_ID"]["value"] })
     except:
         lake_dict[lake["lake"]["value"].strip('http://www.wikidata.org/entity/')].update({ "geoname_id" : None })
